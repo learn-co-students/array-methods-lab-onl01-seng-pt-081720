b695042541e7ea5bd03e2211741052fab6824ec2
Jazmin Matos <jmatos1993@gmail.com>
2020-08-21 23:39:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
+#1
 def using_include(array, element)
-
+  array.include?(element)
 end
 
+#2
 def using_sort(array)
-
+  array.sort
 end
 
+#3
 def using_reverse(array)
-
+  array.reverse
 end
 
+#4
 def using_first(array)
-
+  array.first
 end
 
+#5
 def using_last(array)
-
+  array.last
 end
 
+#6
 def using_size(array)
-
+  array.size
 end
